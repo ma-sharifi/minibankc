cb50e874ad8874e35284ab078d37780150aa8446
Mahdi Sharifi <mahdi.elu@gmail.com>
2022-04-26 18:08:12 +0430

    Add project structure

---
+++ src/main/java/com/example/minibankc/MinibankcApplication.java
@@ -0,0 +1,13 @@
+package com.example.minibankc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibankcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibankcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minibankc/MinibankcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minibankc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibankcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
