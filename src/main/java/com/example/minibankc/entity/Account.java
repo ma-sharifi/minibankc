bee4a8eadd54f608ec049f173f328d703db765c3
Mahdi Sharifi <mahdi.elu@gmail.com>
2022-04-26 18:24:04 +0430

    Add Account Entity

---
+++ src/main/java/com/example/minibankc/entity/Account.java
@@ -0,0 +1,79 @@
+package com.example.minibankc.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * save customers account data
+ */
+@Entity
+@Table(name = "account")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account extends BaseEntity {
+
+    @NotNull
+    @Column(name = "balance", nullable = false)
+    private long balance;
+
+    //https://vladmihalcea.com/the-best-way-to-map-a-onetomany-association-with-jpa-and-hibernate/
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties(value = { "account" }, allowSetters = true)
+    private Set<AccountTransaction> accountTransactions = new HashSet<>();
+
+    @ManyToOne
+    @JsonIgnoreProperties(value = { "account" }, allowSetters = true)
+    @JsonIgnore
+    private Customer customer;
+
+    public Account(Customer customer) {
+        this.customer = customer;
+    }
+    public void addTransaction(AccountTransaction transaction) {
+        accountTransactions.add(transaction);
+        balance= balance+transaction.getAmount();
+        transaction.setNewBalance(balance);
+        transaction.setAccount(this);
+    }
+
+    public void removeTransaction(AccountTransaction transaction) {
+        accountTransactions.remove(transaction);
+        transaction.setAccount(null);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof com.example.minibank.entity.Account)) {
+            return false;
+        }
+        return super.getId() != null && super.getId().equals(((com.example.minibank.entity.Account) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Account{" +
+            "id=" + getId() +
+            ", balance=" + getBalance() +
+            ", createdAt='" + getCreatedAt() + "'" +
+            "}";
+    }
+}
--- src/main/java/com/example/minibankc/repository/CustomerRepository.java
+++ src/main/java/com/example/minibankc/repository/CustomerRepository.java
@@ -1,9 +1,15 @@
 package com.example.minibankc.repository;
 
-import com.example.minibank.entity.Customer;
+import com.example.minibankc.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author Mahdi Sharifi
+ * @version 2022.1.1
+ * https://www.linkedin.com/in/mahdisharifi/
+ * @since 4/24/22
+ */
 /**
  * Spring Data SQL repository for the Customer entity.
  */
