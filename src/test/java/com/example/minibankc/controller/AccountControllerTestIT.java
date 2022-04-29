97253f38793719d5e28461db4ba22278b4ad6511
Mahdi Sharifi <mahdi.elu@gmail.com>
2022-04-29 16:00:05 +0430

    Add Banner and AccountControllerIT

---
+++ src/test/java/com/example/minibankc/controller/AccountControllerTestIT.java
@@ -0,0 +1,10 @@
+package com.example.minibankc.controller;
+
+/**
+ * @author Mahdi Sharifi
+ * @version 2022.1.1
+ * https://www.linkedin.com/in/mahdisharifi/
+ * @since 4/29/22
+ */
+public class AccountControllerTestIT {
+}
